a6227b8d2ac495909d4309e3aa5ca864a8eadd38
Mina Kim <rmaomina@gmail.com>
2023-01-14 20:42:59 +0900

    feat: useParams and Outlet example

---
+++ src/data/db.ts
@@ -0,0 +1,29 @@
+const members = [
+  {
+    userId: 1,
+    name: '에비',
+    profile: '안녕하세요, 에비입니다.',
+  },
+  {
+    userId: 2,
+    name: '제인',
+    profile: '안녕하세요, 제인입니다.',
+  },
+  {
+    userId: 3,
+    name: '그레이스',
+    profile: '안녕하세요, 그레이스입니다.',
+  },
+  {
+    userId: 4,
+    name: '제시',
+    profile: '안녕하세요, 제시입니다.',
+  },
+  {
+    userId: 5,
+    name: '미카',
+    profile: '안녕하세요, 미카입니다.',
+  },
+];
+
+export default members;
